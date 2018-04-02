fbc36672675c9177343b1ef090db5c60b37062f3
Himanshu-skb-oo7 <Himanshu.skb.oo7@gmail.com>
2018-04-03 04:50:33 +0530

    Add Link & Image Share options

--- background.js
+++ background.js
@@ -1,9 +1,39 @@
-chrome.contextMenus.create({
-    title:"Twitter Social Toolkit ",
-    contexts: ["selection"],
-    onclick: myfunction
-});
+var contextList = ["selection","link","image","page"];
 
-function myfunction(selectedText) {
-    chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+selectedText.selectionText})
+for(i=0;i<contextList.length;i++)
+{
+    var context= contextList[i];
+    var titleX= "Twitter Social ToolKit for "+context+" on your twitter profile";
+
+    chrome.contextMenus.create({
+        title:titleX,
+        contexts: [context],
+        onclick: myfunction,
+        id: context
+    });
+
+}
+
+
+
+function myfunction(data) {
+    switch (data.menuItemId){
+        case 'selection':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+data.selectionText})
+            break;
+
+        case 'link':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?url="+data.linkUrl})
+            break;
+
+        case 'image':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?url="+data.srcUrl})
+            break;
+
+        case 'page':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text=Mypage"})
+
+
+    }
+
 }

e3eed9127a5578645018d67d0ddb9c1ea551eb96
Himanshu-skb-oo7 <Himanshu.skb.oo7@gmail.com>
2018-04-03 07:27:47 +0530

    Added Facebook Sharing Options

--- background.js
+++ background.js
@@ -1,39 +1,70 @@
-var contextList = ["selection","link","image","page"];
+var contextListTwitter = ["selection","link","image","page"];
 
-for(i=0;i<contextList.length;i++)
-{
-    var context= contextList[i];
-    var titleX= "Twitter Social ToolKit for "+context+" on your twitter profile";
+for(i=0;i<contextListTwitter.length;i++) {
+    var context = contextListTwitter[i];
+    var titleX = "Share On Twitter";
 
     chrome.contextMenus.create({
-        title:titleX,
+        title: titleX,
         contexts: [context],
         onclick: clickHandler,
-        id: context
+        id: context+"Twitter"
+    });
+
+
+
+    var titleX = "Share On Facebook";
+    chrome.contextMenus.create({
+        title: titleX,
+        contexts: [context],
+        onclick: clickHandler,
+        id: context+"Facebook"
     });
 
 }
 
 
-
 function clickHandler(data,tab) {
     switch (data.menuItemId){
-        case 'selection':
+        case 'selectionTwitter':
             chrome.windows.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(data.selectionText),type:"panel"})
             break;
 
-        case 'link':
+        case 'linkTwitter':
             chrome.windows.create({url: "https://twitter.com/intent/tweet?url="+encodeURIComponent(data.linkUrl),type:"panel"})
             break;
 
-        case 'image':
+        case 'imageTwitter':
             chrome.windows.create({url: "https://twitter.com/intent/tweet?url="+encodeURIComponent(data.srcUrl),type:"panel"})
             break;
 
-        case 'page':
+        case 'pageTwitter':
             chrome.windows.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+(data.pageUrl),type:"panel"})
+            break;
 
+        case 'selectionFacebook':
+            chrome.windows.create({url: "https://www.facebook.com/sharer/sharer.php?&u="+data.pageUrl+"&quote="+data.selectionText,type:"panel"})
+            break;
+
+        case 'pageFacebook':
+            chrome.windows.create({url: "https://www.facebook.com/sharer/sharer.php?&u="+data.pageUrl+"&quote=",type:"panel"})
+            break;
+
+        case 'linkFacebook':
+            chrome.windows.create({url: "https://www.facebook.com/sharer/sharer.php?&u="+data.linkUrl+"&quote=",type:"panel"})
+            break;
+
+
+        case 'imageFacebook':
+            chrome.windows.create({url: "https://www.facebook.com/sharer/sharer.php?&u="+data.pageUrl+"&quote=",type:"panel"})
+            break;
 
     }
 
 }
+
+chrome.runtime.onMessage.addListener(function (response,sender,sendResponse) {
+    if(response=="Facebook") {
+        window.location.assign("quora.com");
+    }
+})
